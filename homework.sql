6530323f34fbd9cdc38ba3c5e7ab71132739fd56
PurnimaChandel <43767250+PurnimaChandel@users.noreply.github.com>
2018-12-15 21:19:35 -0500

    Update homework.sql

--- homework.sql
+++ homework.sql
@@ -28,6 +28,7 @@ WHERE COUNTRY IN ('Afghanistan', 'Bangladesh', 'China');
 #3a. You want to keep a description of each actor. You don't think you will be performing queries on a description, so create a column in the table actor named description and use the data type BLOB (Make sure to research the type BLOB, as the difference between it and VARCHAR are significant).
 ALTER TABLE SAKILA.ACTOR
 ADD DESCRIPTION BLOB;
+
 #3b. Very quickly you realize that entering descriptions for each actor is too much effort. Delete the description column.
 ALTER TABLE SAKILA.ACTOR
 DROP COLUMN DESCRIPTION;
@@ -36,6 +37,7 @@ DROP COLUMN DESCRIPTION;
 SELECT LAST_NAME, COUNT(LAST_NAME) AS COUNT
 FROM SAKILA.ACTOR
 GROUP BY LAST_NAME;
+
 #4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors.
 SELECT LAST_NAME, COUNT(LAST_NAME) AS COUNT
 FROM SAKILA.ACTOR
@@ -76,12 +78,13 @@ FROM SAKILA.FILM
 INNER JOIN SAKILA.FILM_ACTOR
 ON FILM.FILM_ID = FILM_ACTOR.FILM_ID
 GROUP BY FILM.TITLE;
+
 #6d. How many copies of the film Hunchback Impossible exist in the inventory system?
 SELECT TITLE, COUNT(INVENTORY_ID) AS TOTAL_COUNT
 FROM SAKILA.FILM
 LEFT JOIN SAKILA.INVENTORY
 ON FILM.FILM_ID = INVENTORY.FILM_ID
-WHERE FILM.TITLE = "Hunchback Impossible"
+WHERE FILM.TITLE = "Hunchback Impossible";
 
 #6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name:
 SELECT FIRST_NAME, LAST_NAME, SUM(AMOUNT) AS TOTAL_AMOUNT
@@ -92,10 +95,55 @@ GROUP BY CUSTOMER.CUSTOMER_ID
 ORDER BY CUSTOMER.LAST_NAME;
 
 #7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters K and Q have also soared in popularity. Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
+SELECT TITLE
+FROM SAKILA.FILM
+WHERE TITLE LIKE "K%" OR "Q%" AND LANGUAGE_ID IN
+(
+SELECT LANGUAGE_ID FROM SAKILA.LANGUAGE
+WHERE NAME ="ENGLISH"
+);
 
 #7b. Use subqueries to display all actors who appear in the film Alone Trip.
+SELECT FIRST_NAME, LAST_NAME
+FROM SAKILA.ACTOR
+WHERE ACTOR_ID in
+(
+SELECT ACTOR_ID 
+FROM SAKILA.FILM_ACTOR
+WHERE FILM_ID IN
+(
+SELECT FILM_ID 
+FROM SAKILA.FILM
+WHERE TITLE ="ALONE TRIP"
+)
+);
+
 #7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. Use joins to retrieve this information.
+SELECT FIRST_NAME, LAST_NAME, EMAIL
+FROM SAKILA.CUSTOMER
+JOIN SAKILA.ADDRESS
+ON CUSTOMER.CUSTOMER_ID = ADDRESS.ADDRESS_ID
+JOIN SAKILA.CITY
+ON ADDRESS.CITY_ID = CITY.CITY_ID
+JOIN SAKILA.COUNTRY
+ON CITY.COUNTRY_ID = COUNTRY.COUNTRY_ID
+WHERE COUNTRY = "CANADA"
+GROUP BY CUSTOMER_ID;
+
 #7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as family films.
+SELECT TITLE
+FROM SAKILA.FILM
+WHERE FILM_ID in
+(
+SELECT FILM_ID
+FROM SAKILA.FILM_CATEGORY
+WHERE CATEGORY_ID IN
+(
+SELECT CATEGORY_ID
+FROM SAKILA.CATEGORY
+WHERE NAME ="FAMILY"
+)
+);
 #7e. Display the most frequently rented movies in descending order.
 #7f. Write a query to display how much business, in dollars, each store brought in.
 #7g. Write a query to display for each store its store ID, city, and country.
